2a1081450cf3812d7272092d6a92f73b4c276274
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2018-01-12 18:00:11 +0300

    added Context shared logic

---
+++ Context.hs
@@ -0,0 +1,60 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module W7W.Context where
+
+import Control.Applicative (Alternative (..))
+
+import Hakyll
+
+import Data.Monoid ((<>), mempty)
+
+import W7W.MultiLang
+
+import W7W.Utils
+
+--
+--
+-- utils
+--
+--
+
+hasMetadataField :: String -> Metadata -> Bool
+hasMetadataField fName m =
+  case lookupString fName m of
+    Just _ -> True
+    Nothing -> False
+
+hasItemField :: MonadMetadata m => String -> Item a -> m Bool
+hasItemField fName =
+  withItemMetadata $ hasMetadataField fName
+
+
+withItemMetadata :: MonadMetadata m => (Metadata -> b) -> Item a -> m b
+withItemMetadata f item = do
+  m <- getMetadata (itemIdentifier item)
+  return $ f m
+
+
+boolFieldM :: String -> (Item a -> Compiler Bool) -> Context a
+boolFieldM name f = field name $ \i -> do
+                      b <- f i
+                      if b
+                        then pure $ error $ unwords $
+                                 ["no string value for bool field:",name]
+                        else empty
+
+--
+--
+-- fields
+--
+--
+
+
+fieldRootUrl =
+  field "root_url" getRootUrl
+  where
+    getRootUrl i = return $ "/" ++ (itemLang i) ++ "/" ++ (itemYear i) ++ "/"
+
+fieldCanonicalName = field "canonical_name" $ return . itemCanonicalName
+
+fieldLang = field "lang" $ return . itemLang
