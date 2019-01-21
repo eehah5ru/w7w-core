568b8b784853914243224a6e515ee256d4f1069d
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2019-01-22 02:40:32 +0300

    cached picture color and exifinfo

--- src/Pictures/Rules.hs
+++ src/Pictures/Rules.hs
@@ -7,8 +7,10 @@ import Data.Monoid ((<>))
 import qualified Data.Text as T
 
 import System.FilePath
+import System.FilePath.Posix ((</>), takeBaseName)
 
 import Hakyll
+import Hakyll.Core.Compiler.Internal (compilerThrow, compilerAsk, compilerConfig)
 
 import W7W.Utils
 import W7W.Context
@@ -18,24 +20,54 @@ import W7W.Pictures.Utils
 -- import W7W.MultiLang (localize, itemLocale, isLocalized, Localized, IsLocalized)
 
 
+-- old version
+-- picturesRules :: (Int, Int) -> Pattern -> Rules ()
+-- picturesRules (w, h) p = do
+--   match p $ do
+--     route (customRoute pictureRoute)
+--     let cmd = "convert"
+--     let args = [ "-"
+--                    , "-resize"
+--                    , concat [show w, "x", show h, "^"]
+--                    -- , "-gravity"
+--                    -- , "Center"
+--                    -- , "-crop"
+--                    -- , concat [show h, "x", show h, "+0+0"]
+--                    -- , "+repage"
+--                    , "-quality"
+--                    , "79"
+--                    , "-" ]
+--     compile $ getResourceLBS >>= withItemBody (unixFilterLBS cmd args)
+--   where
+--     pictureRoute i =
+--       let path = toFilePath i
+--       in (replaceExtension path) "jpg"
+
+
 picturesRules :: (Int, Int) -> Pattern -> Rules ()
 picturesRules (w, h) p = do
   match p $ do
     route (customRoute pictureRoute)
-    let cmd = "convert"
-    let args = [ "-"
-                   , "-resize"
-                   , concat [show w, "x", show h, "^"]
-                   -- , "-gravity"
-                   -- , "Center"
-                   -- , "-crop"
-                   -- , concat [show h, "x", show h, "+0+0"]
-                   -- , "+repage"
-                   , "-quality"
-                   , "79"
-                   , "-" ]
-    compile $ getResourceLBS >>= withItemBody (unixFilterLBS cmd args)
+    compile $ convertItem
   where
+    
+    convertItem = do
+      i <- getUnderlying
+      dstBase <- return . destinationDirectory . compilerConfig =<< compilerAsk
+      destPath <- getUnderlying 
+                  >>= getRoute >>= maybe (compilerThrow $ ["no route for: " ++ (toFilePath i)] ) return >>= return . (</>) dstBase
+      srcPath <- getResourceFilePath
+      r0 <- unsafeCompiler (makeDirectories destPath)
+      r <- (unixFilter "convert"
+                      [ srcPath
+                      , "-resize"
+                      , concat [show w, "x", show h, "^"]
+                      , "-quality"
+                      , "79"
+                      , destPath]
+                      "")
+      makeItem ()
+
     pictureRoute i =
       let path = toFilePath i
       in (replaceExtension path) "jpg"
